bf1e11d4221750a0d798aafd7f81664614aaca5b
Stanyslas Fruchard <stanyslas.fruchard@etu-webschoolfactory.fr>
2016-10-05 12:03:44 +0200

    Tweet + User first route

--- apis/tweets/index.js
+++ apis/tweets/index.js
@@ -8,10 +8,26 @@ const models = require('../../database');
  * Export the tweet API with its own routes.
  */
 
-exports.register = function (server, options, next) {
+ exports.register = function (server, options, next) {
+  server.route({
+    method:'POST',
+    path:'/tweets',
+    handler:(request, reply) => {
+      const data = request.payload || request.params || request.body;
+      models.tweet.create(data)
+       .then(result => {
+         return reply(result)
+       })
+       .catch(err => {
+         return reply({
+           error : err.message
+         })
+       })
+    }
+  })
+   next();
+ };
 
-  next();
-};
 
 exports.register.attributes = {
   name: 'tweets'
